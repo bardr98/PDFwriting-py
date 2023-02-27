8f0f9634159ae12408c7e35db46d28a7a9d133f8
Bar <91567022+bardr98@users.noreply.github.com>
2023-02-28 01:15:41 +0200

    Update PDFwriting.py

--- PDFwriting.py
+++ PDFwriting.py
@@ -9,12 +9,6 @@ reader = PdfReader(r"your_pdf_file_path")
 # printing number of pages in pdf file
 print(len(reader.pages))
 
-# getting a specific page from the pdf file
-page = reader.pages[0]
-
-# extracting text from page
-text = page.extract_text()
-
 # Create an image object
 width, height = int(8.27 * 200), int(11.7 * 200)  # A4 at 200dpi
 image = Image.new(mode='RGB', size=(width, height), color='white')
@@ -36,20 +30,26 @@ draw = ImageDraw.Draw(image)
 x = 50
 y = 50
 
-# Write the text to the image using the custom font
-draw.text((x, y), text, font=font, fill='black')
+# Loop through each page of the PDF file
+for page_num, page in enumerate(reader.pages):
 
-# Save the image as a JPEG file
-image_file = 'handwritten.jpg'
-image.save(image_file)
+    # extracting text from page
+    text = page.extract_text()
 
-# Convert the image to PDF using PIL
-pdf_file = 'handwritten.pdf'
-image_pdf = Image.open(image_file)
-image_pdf.save(pdf_file)
+    # Write the text to the image using the custom font
+    draw.text((x, y), text, font=font, fill='black')
 
-# Remove the temporary JPEG image file
-os.remove(image_file)
+    # Save the image as a JPEG file
+    image_file = f'handwritten_page{page_num+1}.jpg'
+    image.save(image_file)
 
-# Open the PDF file
-os.startfile(pdf_file)
+    # Convert the image to PDF using PIL
+    pdf_file = f'handwritten_page{page_num+1}.pdf'
+    image_pdf = Image.open(image_file)
+    image_pdf.save(pdf_file)
+
+    # Remove the temporary JPEG image file
+    os.remove(image_file)
+
+    # Open the PDF file
+    os.startfile(pdf_file)
